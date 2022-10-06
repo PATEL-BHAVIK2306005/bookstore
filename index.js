4c3b4af1526e905a38c93206c7873e7dec81610f
Galaxor2121 <jefjef713@gmail.com>
2022-10-06 22:40:36 +0300

    Added Connection String

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose');
-
+const connectionString = "mongodb+srv://Galaxor:6HfUAvf8t8FS2Suh@cluster0.vzltvty.mongodb.net/?retryWrites=true&w=majority"
 const app = express()
 const port = 3000
+
+mongoose.connect(connectionString).then((ans) => {
+  console.log("ConnectedSuccessful")
+}).catch((err) => {
+  console.log("Error in the Connection")
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
