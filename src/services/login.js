f48d0f66bba5342e0e16e04ea0b6c391c45acc72
Galaxor2121 <jefjef713@gmail.com>
2022-11-03 21:58:10 +0200

    fixed service2

--- src/services/login.js
+++ src/services/login.js
@@ -1,13 +1,13 @@
 const {UserModel} = require("../models");
 
 async function login(username, password) {
-    const user = await UserModel.findOne({ _id: username, password });
+    const user = await UserModel.findOne({ username, password });
     return user != null
 }
-async function isAdmin(name) {
+async function isAdmin(username) {
     if (username != null)
     {
-        const user = (await UserModel.find({username: name}))[0]
+        const user = (await UserModel.find({username}))[0]
         if (user.role == "Administrator")
             return true
         else
